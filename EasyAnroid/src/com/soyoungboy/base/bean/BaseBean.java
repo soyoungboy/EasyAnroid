5255d7e758542fb324317d47570b2c81528a89d8
soyoungboy <wang12fu49bin>
2015-08-14 15:21:00 +0800

    [新增] 新增BaseBean里面getBeanFromJson这个方法，自动解析json为javaBean的对象

--- EasyAnroid/src/com/soyoungboy/base/bean/BaseBean.java
+++ EasyAnroid/src/com/soyoungboy/base/bean/BaseBean.java
@@ -2,6 +2,8 @@ package com.soyoungboy.base.bean;
 
 import java.io.Serializable;
 
+import com.soyoungboy.base.util.GsonImpl;
+
 /**
  * @类名: BaseBean
  * @描述: TODO(BaseBean 所有Bean都需要继承该类,继承自该类的bean都以实现序列化)
@@ -9,4 +11,9 @@ import java.io.Serializable;
  */
 public class BaseBean implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	public <T> T getBeanFromJson(String response, Class<T> T) {
+		T t = GsonImpl.get().toObject(response, T);
+		return t;
+	}
 }
