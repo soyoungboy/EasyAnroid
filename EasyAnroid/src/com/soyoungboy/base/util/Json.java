25f873bb76b3ed86d8bef602742bacf9b64a92a4
soyoungboy <1213854220@qq.com>
2015-12-26 20:27:11 +0800

    Update Json.java

--- EasyAnroid/src/com/soyoungboy/base/util/Json.java
+++ EasyAnroid/src/com/soyoungboy/base/util/Json.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * TODO: json工具类
  * 
- * @author soyoungboy
+ * @author 此处绝大部分代码来自lite的工具类，只添加了自己项目中需要的一个方法，此处标注下
  * @date 2014-11-8 下午2:32:24
  */
 public abstract class Json {
